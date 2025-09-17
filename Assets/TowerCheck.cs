40ccc72a1cfc544115c30fc25ef20df2665559c7
seophugo <hugo.lanting@gmail.com>
2025-09-17 14:36:47 +0200

    ff buttons gefixd

--- Assets/TowerCheck.cs
+++ Assets/TowerCheck.cs
@@ -1,27 +1,58 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Mousecheck : MonoBehaviour
 {
     [SerializeField] private LayerMask _rayLayer;
-    [SerializeField] private GameObject _tower;
+    [SerializeField] private Material _selectedMaterial;
+    [SerializeField] private bool _allowMultipleSpawns = true;
 
-    private float _distance = Mathf.Infinity;
-    void Start()
-    {
-
-    }
+    private GameObject _selectedObject;
+    private Material _originalMaterial;
+    private bool _hasSpawned = false; // houdt bij of er al iets gespawned is
 
     void Update()
     {
+        // Als meerdere spawns niet toegestaan zijn en er al gespawned is, mag niet selecteren
+        if (!_allowMultipleSpawns && _hasSpawned) return;
+
         if (Input.GetMouseButtonDown(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
-            if (Physics.Raycast(ray, out hit, _distance, _rayLayer))
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, _rayLayer))
             {
-                Instantiate(_tower, hit.transform.position, Quaternion.identity);
+                GameObject clickedObject = hit.transform.gameObject;
+
+                // deselect als dezelfde Cube wordt aangeklikt
+                if (_selectedObject == clickedObject)
+                {
+                    _selectedObject.GetComponent<Renderer>().material = _originalMaterial;
+                    _selectedObject = null;
+                    return;
+                }
+
+                if (_selectedObject != null)
+                {
+                    _selectedObject.GetComponent<Renderer>().material = _originalMaterial;
+                }
+
+                _selectedObject = clickedObject;
+                _originalMaterial = _selectedObject.GetComponent<Renderer>().material;
+                _selectedObject.GetComponent<Renderer>().material = _selectedMaterial;
             }
         }
+    }
+
+    public void SpawnTower(GameObject towerPrefab)
+    {
+        if (_selectedObject == null) return;
+
+        Instantiate(towerPrefab, _selectedObject.transform.position, Quaternion.identity);
+
+        _selectedObject.GetComponent<Renderer>().material = _originalMaterial;
+        _selectedObject = null;
+
+        _hasSpawned = true; // voorkomt nieuwe spawns als _allowMultipleSpawns = false
     }
 }
